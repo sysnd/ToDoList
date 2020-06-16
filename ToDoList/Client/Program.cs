c0b75f7949a319ca84d3f861ef9f3c61b6f7ec71
nia.dryankova <nia.dryankova@gmail.com>
2020-06-16 17:49:05 +0300

    Initial layout for the view and upsert components for users and tasks

--- ToDoList/Client/Program.cs
+++ ToDoList/Client/Program.cs
@@ -1,12 +1,9 @@
+using Blazored.Modal;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace ToDoList.Client
 {
@@ -18,6 +15,8 @@ namespace ToDoList.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBlazoredModal();
 
             await builder.Build().RunAsync();
         }
